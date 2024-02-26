a3ec49a38a7ac00f8d41619579dbff77bdb90b26
Precious Chika <43546663+pre22@users.noreply.github.com>
2024-02-26 10:30:02 +0100

    added app urls to project level route

--- ridesharingapp/urls.py
+++ ridesharingapp/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('locations.urls')),
+    path('locations/', include('locations.urls')),
+    path("notifications/", include('notifications.urls')),
 ]
