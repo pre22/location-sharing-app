6bba86ecd8711f127a4abb63d427f7f15cbc5952
Precious Chika <43546663+pre22@users.noreply.github.com>
2024-02-26 09:55:34 +0100

    updated asgi

--- ridesharingapp/asgi.py
+++ ridesharingapp/asgi.py
@@ -10,7 +10,19 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from locations.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ridesharingapp.settings')
 
-application = get_asgi_application()
+# application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AllowedHostsOriginValidator(
+            URLRouter(
+                websocket_urlpatterns
+            )
+        ),
+})
