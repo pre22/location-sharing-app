33373739abbce3698b2cf7f8d4c182e4a94dc996
Precious Chika <43546663+pre22@users.noreply.github.com>
2024-02-26 09:50:34 +0100

    updated settings

--- ridesharingapp/settings.py
+++ ridesharingapp/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
 
     'locations.apps.LocationsConfig',
     'users.apps.UsersConfig',
+
+    'daphne',
 ]
 
 
@@ -77,6 +79,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ridesharingapp.wsgi.application'
 
 
+ASGI_APPLICATION = 'ridesharingapp.asgi.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': "channels.layers.InMemoryChannelLayer"
+    }
+}
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
