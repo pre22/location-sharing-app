7a5d16d50881c672cf82a2fafd28ced28a2977fa
Precious Chika <43546663+pre22@users.noreply.github.com>
2024-02-27 18:43:50 +0100

    updated notifications model

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from utils.models import BaseModel
 
@@ -5,8 +6,10 @@ from users.models import CustomUser
 
 # Create your models here.
 class Notification(BaseModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     message = models.CharField(max_length=100)
+    delivered =  models.BooleanField(default=False)
     
     def __str__(self):
         return self.message
