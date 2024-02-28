4862ff7106952e159cfc0479091bac040aef1f60
Precious Chika <43546663+pre22@users.noreply.github.com>
2024-02-28 22:33:28 +0100

    created models for both peers

--- users/models.py
+++ users/models.py
@@ -1,11 +1,27 @@
 from django.db import models
-
+from utils.models import BaseModel
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    pass
+    is_peer_one = models.BooleanField(default=False)
+    is_peer_two = models.BooleanField(default=False)
 
     def __str__(self):
         return self.email
+    
+
+class PeerA(BaseModel):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.email
+    
+
+class PeerB(BaseModel):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.email
+
