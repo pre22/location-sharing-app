87a1863d9f1446d79948cdbf72c312716a32ef33
Precious Chika <43546663+pre22@users.noreply.github.com>
2024-02-28 22:34:30 +0100

    created model for location sharing request

--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,22 @@
+import uuid
 from django.db import models
+from utils.models import BaseModel
 
-# Create your models here.
+from users.models import PeerA, PeerB
+
+STATUS = (
+    ("IN-PROGRESS", "IN-PROGRESS"),
+    ("WAITING", "WAITING"),
+    ("CANCELLED", "CANCELLED"),
+    ("COMPLETED", "COMPLETED"),
+)
+
+class ShareLocationRequestModel(BaseModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    peer_a = models.ForeignKey(PeerA, on_delete=models.CASCADE)
+    peer_b = models.ForeignKey(PeerB, on_delete=models.CASCADE)
+    status = models.CharField(choices=STATUS, max_length=50)
+
+    def __str__(self):
+        return self.id
+
