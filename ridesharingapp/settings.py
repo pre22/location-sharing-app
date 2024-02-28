4436edc8ebdbabadb3e2d72cbab36cac84af9642
Precious Chika <43546663+pre22@users.noreply.github.com>
2024-02-28 18:02:25 +0100

    abstracted local database credentials

--- ridesharingapp/settings.py
+++ ridesharingapp/settings.py
@@ -107,11 +107,11 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT')
     }
     
 }
