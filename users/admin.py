8386b6bb0886ac794b31edc79029d49f98d145ee
Precious Chika <43546663+pre22@users.noreply.github.com>
2024-03-02 14:32:45 +0100

    added model to admin

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, PeerA, PeerB
 # Register your models here.
 admin.site.register(CustomUser)
+admin.site.register(PeerA)
+admin.site.register(PeerB)
